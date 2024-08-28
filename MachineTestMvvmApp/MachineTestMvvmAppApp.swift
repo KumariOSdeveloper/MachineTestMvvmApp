5656ade12675806d97499223e8dcdec3dbbbacd6
kumarmahesh3023 <73269819+kumarmahesh3023@users.noreply.github.com>
2024-08-28 16:02:12 +0530

    Initial Commit

---
+++ MachineTestMvvmApp/MachineTestMvvmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MachineTestMvvmAppApp.swift
+//  MachineTestMvvmApp
+//
+//  Created by Mahesh Kumar on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MachineTestMvvmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
